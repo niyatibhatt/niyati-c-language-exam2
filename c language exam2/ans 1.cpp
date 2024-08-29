12c50a6250934f108e0fbc72ea57ba1cf78d3a39
niyatibhatt <niyatibhatt315@gmail.com>
2024-08-29 11:24:39 +0530

    Add files via upload

---
+++ c language exam2/ans 1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+void main(){
+              
+    char a[5] = {'A','B','C','D','E'};
+
+    printf("Array 1 value : %c\n",a[5]);
+
+
+               
+    int b[5] = {100,200,300,400,500};
+
+    printf("Array 2 value : %d\n",b[3]);
+
+    int c[10];
+
+    printf("Enter a number\n");
+
+    scanf("%d",&c[0]);
+
+    printf("Array 3 value : %d",c[0]);
+
+
+
+}
